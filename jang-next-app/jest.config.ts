1451dc4a97d1dbfb13e49d9b34a9b8bcf48f94f4
lee-jangwon <leejangwon1995@gmail.com>
2024-10-06 22:53:33 -0500

    chore: add nest and next app
    넥스트와 네스트 어플리케이션 플러그인 추가

---
+++ jang-next-app/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'jang-next-app',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/jang-next-app',
+};
